4bb0d136fbaf3f29ded747167df4b61e3467cad8
AlNieto <alexduteich@gmail.com>
2020-06-24 18:16:56 +0200

    Create teamDrawer + team server state

--- src/components/Bracket.vue
+++ src/components/Bracket.vue
@@ -1,24 +1,33 @@
 <template>
   <v-container fluid>
+    <team-drawer>
       <bracket-node v-if="bracket.bracketNode" :node="bracket.bracketNode"></bracket-node>
+    </team-drawer>
   </v-container>
 </template>
 
 <script lang="ts">
 import { PropType } from "vue";
 import BracketNode from "./BracketNode.vue";
+import TeamDrawer from "./TeamDrawer.vue";
 import IBracket from "@/models/IBracket";
 
 export default {
   name: "Bracket",
   components: {
-    BracketNode
+    BracketNode,
+    TeamDrawer
   },
   props: {
     bracket: {
       type: Object as PropType<IBracket>,
       required: true
     }
+  },
+  data() {
+    return {
+      drawer: null,
+    };
   }
 };
 </script>
--- src/store/TeamStore.ts
+++ src/store/TeamStore.ts
@@ -6,7 +6,60 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    teams: Array<ITeam>()
+    teams: [
+      {
+        id: "1",
+        name: "Jackets"
+      },
+      {
+        id: "2",
+        name: "Legs"
+      },
+      {
+        id: "3",
+        name: "United"
+      },
+      {
+        id: "4",
+        name: "Athletic"
+      },
+      {
+        id: "5",
+        name: "Teichois"
+      },
+      {
+        id: "6",
+        name: "BDX"
+      },
+      {
+        id: "7",
+        name: "Spartiates"
+      },
+      {
+        id: "8",
+        name: "Atheniens"
+      },
+      {
+        id: "9",
+        name: "Gayz"
+      },
+      {
+        id: "10",
+        name: "Broncos"
+      },
+      {
+        id: "11",
+        name: "OUPS"
+      },
+      {
+        id: "12",
+        name: "Pirates"
+      },
+      {
+        id: "13",
+        name: "Donkeys"
+      }
+    ]
   },
   mutations: {
     initTeam: (state, teams: ITeam[]) => {
@@ -20,5 +73,10 @@ export default new Vuex.Store({
     }
   },
   actions: {
+  },
+  getters: {
+    getTeamById: (state) => (id: string) => {
+      return state.teams.find(team => team.id === id)
+    }
   }
 })
